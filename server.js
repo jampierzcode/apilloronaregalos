a9e63e2557134911365afafc9c57a280f146d754
jampierzcode <jampierv127@gmail.com>
2024-02-26 19:37:51 -0500

    iniciando servidor regalos llorona

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Ruta para leer los datos del archivo JSON
+app.get("/api/datos", (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync("datos.json", "utf8"));
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error al leer los datos" });
+  }
+});
+// Ruta para leer los datos del archivo JSON
+app.get("/api/datos/:codigo", (req, res) => {
+  try {
+    const codigo = req.params.codigo;
+    const data = JSON.parse(fs.readFileSync("datos.json", "utf8"));
+    const registro = data.regalos.find((reg) => reg.codigoUnico === codigo);
+    if (registro) {
+      res.json(registro);
+    } else {
+      res.status(404).json({ error: "Registro no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al leer los datos" });
+  }
+});
+
+// Ruta para sobreescribir el archivo JSON
+app.put("/api/datos", (req, res) => {
+  try {
+    fs.writeFileSync("datos.json", JSON.stringify(req.body));
+    res.json({ message: "Datos actualizados correctamente" });
+  } catch (error) {
+    res.status(500).json({ error: "Error al actualizar los datos" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
